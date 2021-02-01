9ab6a396b3d1b4d74357c4216eb9d0bf779d7161
Puneetphull <73685489+Puneetphull@users.noreply.github.com>
2021-02-01 10:02:25 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Button } from 'protractor';
+import {AppComponent} from './app.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ButtonComponent } from './button/button.component';
+import { HomeComponent } from './home/home.component';
+import { IconsComponent } from './icons/icons.component';
+import { TagComponent } from './tag/tag.component';
+import { ThemesComponent } from './themes/themes.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent,
+  },{
+ path:'App',
+ component:AppComponent
+},
+{
+  path:'home',
+ component:HomeComponent
+
+},
+{
+ path:'tag',
+ component:TagComponent
+},
+{
+  path:'button',
+  component:ButtonComponent
+},{
+  path:"icons",
+  component:IconsComponent,
+},
+{
+  path:"themes",
+  component:ThemesComponent,
+},
+  {
+    path:"breadcrumb",
+    component:BreadcrumbComponent,
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
